3bdfed5346a1578094b77feebf410c5b5e27b054
mkmunnakr <mkmunnakr2002@gmail.com>
2025-08-20 18:12:46 +0530

    Create intChecked.java

---
+++ intChecked.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class IntChecked {
+    public static void main(String[] args) {
+        System.out.println("Enter Your Number : ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
