4930a1cbdd54a1f759e1228d5a88c7a30742cddc
mkmunnakr <mkmunnakr2002@gmail.com>
2025-08-21 18:42:48 +0530

    Create markPercentage.java

---
+++ markPercentage.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class marksPercentage {
+    public static void main(String[] args) {
+        System.out.println("Calculate Marks Percentage of a given Student");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Marks 1 to 100 Sub1 : ");
+        float a = sc.nextFloat();
+        System.out.print("Enter Marks 1 to 100 Sub2 : ");
+        float b = sc.nextFloat();
+        System.out.print("Enter Marks 1 to 100 Sub3 : ");
+        float c = sc.nextFloat();
+        System.out.print("Enter Marks 1 to 100 Sub4 : ");
+        float d = sc.nextFloat();
+        System.out.print("Enter Marks 1 to 100 Sub5 : ");
+        float e = sc.nextFloat();
+
+        float percentage = (a+b+c+d+e) / 5;
+
+        System.out.print("Percentage of Student in Exam : ");
+        System.out.println(percentage);
+
+    }
+}
