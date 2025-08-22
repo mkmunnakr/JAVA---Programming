f4ff11e99f13b63faa54f78b7501cc44f7b05255
mkmunnakr <mkmunnakr2002@gmail.com>
2025-08-22 19:39:29 +0530

    Update markPercentage.java

--- markPercentage.java
+++ markPercentage.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class marksPercentage {
+public class markPercentage {
     public static void main(String[] args) {
         System.out.println("Calculate Marks Percentage of a given Student");
         Scanner sc = new Scanner(System.in);
