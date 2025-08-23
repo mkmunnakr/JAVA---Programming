86b1fd563563dfbb211adebd9d61a99a4eb4c264
mkmunnakr <mkmunnakr2002@gmail.com>
2025-08-24 01:17:30 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Employee {
+    public static void main(String[] args) {
+        System.out.print("Enter your age : ");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age>55) {
+            System.out.println("Your ar experienced ");
+        }
+        else if(age > 46 ) {
+            System.out.println("Your are semi-experienced ");
+        }
+        else if(age>36) {
+            System.out.println("You are semi-semi-experienced ");
+        }
+        else {
+            System.out.println("You are not experienced ");
+        }
+    }
+}
