8443217c44339eb372e9874048cf874427fd2aa5
mkmunnakr <mkmunnakr2002@gmail.com>
2025-08-24 01:19:37 +0530

    Create literalJava.java

---
+++ literalJava.java
@@ -0,0 +1,13 @@
+public class literalJava {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 67;
+        long ageDino = 566666666666L;
+        boolean a = true;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66D;
+        System.out.println(age);
+    }
+}
