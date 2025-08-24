b880e81767298486fa59d9ca4d2adf0a8fde5f47
mkmunnakr <mkmunnakr2002@gmail.com>
2025-08-25 02:53:33 +0530

    Create basicEqu.java

---
+++ basicEqu.java
@@ -0,0 +1,11 @@
+public class basicEqu {
+    public static void main(String[] args) {
+        int v = 19;
+        int u = 8;
+        int a = 2;
+        int s = 18;
+        int x = (v*v-u*u)/(2*a*s);
+        System.out.println(x);
+
+    }
+}
