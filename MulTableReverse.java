d68fba682a4d1bacc19eb6c3ca5242b9975dfabc
mkmunnakr <mkmunnakr2002@gmail.com>
2025-08-28 01:27:40 +0530

    Create MulTableReverse.java

---
+++ MulTableReverse.java
@@ -0,0 +1,9 @@
+public class MulTableReverse {
+    // Print Multiplication table for 10 reverse order
+    public static void main(String[] args) {
+        int n = 10; // table for 10
+        for(int i=10 ; i>0; i--){
+            System.out.println(n*i);
+        }
+    }
+}
