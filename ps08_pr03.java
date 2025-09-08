c73806e9942e5ff6e2cd7d4e77e13c50a0c5fc02
mkmunnakr <mkmunnakr2002@gmail.com>
2025-09-08 21:49:09 +0530

    Add files via upload

---
+++ ps08_pr03.java
@@ -0,0 +1,18 @@
+
+class Square{
+    int side;
+    public int area(){
+        return side*side;
+    }
+    public int perimeter(){
+        return 4*side;
+    }
+}
+public class ps08_pr03 {
+    public static void main(String[] args) {
+        Square sq = new Square();
+        sq.side = 6;
+        System.out.println(sq.area());
+        System.out.println(sq.perimeter());
+    }
+}
