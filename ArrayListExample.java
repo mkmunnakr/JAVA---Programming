ea661b8086c9d904624015f833848c2e80b03056
mkmunnakr <mkmunnakr2002@gmail.com>
2026-01-31 01:13:43 +0530

    Add files via upload

---
+++ ArrayListExample.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        // Syntax
+        ArrayList<Integer> list = new ArrayList<>(10);
+
+        list.add(673);
+        list.add(670);
+        list.add(675);
+        list.add(678);
+        list.add(676);
+        list.add(679);
+
+        System.out.println(list);
+
+    }
+}
