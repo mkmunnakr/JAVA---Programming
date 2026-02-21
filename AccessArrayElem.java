858f3977d2a4891e474375b35195d64e715b7aff
mkmunnakr <mkmunnakr2002@gmail.com>
2026-02-21 09:52:09 +0530

    Add files via upload

---
+++ AccessArrayElem.java
@@ -0,0 +1,12 @@
+public class AccessArrayElem {
+    public static void main(String[] args){
+
+        int[] arr = {2, 4, 8, 12, 16};
+
+        // Accessing fourth element
+        System.out.print(arr[3] + " ");
+
+        // Accessing first element
+        System.out.print(arr[0]);
+    }
+}
