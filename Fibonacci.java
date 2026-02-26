2f81fd95cc95aab9495ed624d07ab447a7faf0f7
mkmunnakr <mkmunnakr2002@gmail.com>
2026-02-26 08:40:05 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 11; i++) {
+            System.out.println(fibonacciFormula(i));
+        }
+    }
+
+    static int fibonacciFormula(int n) {
+        return (int)((Math.pow(((1 + Math.sqrt(5)) / 2), n) - Math.pow(((1 - Math.sqrt(5))/2),n)) / Math.sqrt(5));
+    }
+
+    static int fibo(int n) {
+        // base condition
+        if ( n < 2) {
+            return n;
+        }
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
